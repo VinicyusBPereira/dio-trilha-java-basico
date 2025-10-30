77e4b65b0a7d53fb888ff3a7b634c59cba28e636
VinicyusBPereira <vinicyusbenp@gmail.com>
2025-10-29 23:20:17 -0300

    cadastro conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o numero da sua conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Digite o numero da agencia:");
+        String agencia = scanner.nextLine();
+        System.out.println("Digite seu nome:");
+        String nome = scanner.nextLine();
+        System.out.println("Digite seu saldo:");
+        double saldo = scanner.nextDouble();
+        scanner.nextLine();
+        
+
+        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+        scanner.close();
     }
 }
